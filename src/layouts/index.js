d58acba29aa638da02fd2e6e88c32bdee18c3ab0
lxy <492861128@qq.com>
2019-08-14 21:06:22 +0800

    clean

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -22,7 +22,7 @@ class BasicLayout extends Component {
           <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
             <div className="logo" />
             <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-              <Menu.Item key="1">
+              {/* <Menu.Item key="1">
                 <Icon type="pie-chart" />
                 <span>Option 1</span>
               </Menu.Item>
@@ -58,20 +58,17 @@ class BasicLayout extends Component {
               <Menu.Item key="9">
                 <Icon type="file" />
                 <span>File</span>
-              </Menu.Item>
+              </Menu.Item> */}
             </Menu>
           </Sider>
           <Layout>
             <Header style={{ background: '#fff', padding: 0 }} />
             <Content style={{ margin: '0 16px' }}>
-              <Breadcrumb style={{ margin: '16px 0' }}>
+              {/* <Breadcrumb style={{ margin: '16px 0' }}>
                 <Breadcrumb.Item>User</Breadcrumb.Item>
                 <Breadcrumb.Item>Bill</Breadcrumb.Item>
-              </Breadcrumb>
-              <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-                Bill is a cat.
-                {children}
-              </div>
+              </Breadcrumb> */}
+              <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>{children}</div>
             </Content>
             <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
           </Layout>
