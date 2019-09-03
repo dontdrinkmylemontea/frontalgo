154c052d9def0e4cdc0286a6e703915f208f9492
lxy <492861128@qq.com>
2019-09-03 16:32:04 +0800

    Layout带登录验证功能

---
+++ src/layouts/MenuData.js
@@ -0,0 +1,8 @@
+export default [
+  {
+    index: 0,
+    name: 'home',
+    path: '/',
+    icon: 'home',
+  },
+];
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,78 +1,101 @@
 import styles from './index.less';
 import React, { Component } from 'react';
-import { Layout, Menu, Icon, Breadcrumb } from 'antd';
-const { Header, Footer, Sider, Content } = Layout;
-const { SubMenu } = Menu;
+import { connect } from 'dva';
+import router from 'umi/router';
+import MenuData from './MenuData';
+import { Layout, Menu, Icon, message } from 'antd';
+import { getCookie, setCookie, checkAuth } from '@/utils/util';
+const { Header, Sider, Content } = Layout;
 
+@connect(({ loading, auth }) => ({
+  loadingData: loading,
+  auth,
+}))
 class BasicLayout extends Component {
+  constructor(props) {
+    super(props);
+    const { dispatch } = this.props;
+    console.log('checkauth', checkAuth(getCookie('username'), getCookie('password')));
+    if (checkAuth(getCookie('username'), getCookie('password'))) {
+      dispatch({ type: 'auth/setauth', payload: { auth: 1 } });
+      router.push('/');
+      console.log('已登录');
+    } else {
+      dispatch({ type: 'auth/setauth', payload: { auth: -1 } });
+    }
+  }
+
   state = {
     collapsed: false,
   };
 
   onCollapse = collapsed => {
-    console.log(collapsed);
     this.setState({ collapsed });
   };
 
+  onClickMenu = ({ key }) => {
+    router.push(MenuData.filter(data => String(data.index) === key)[0].path);
+  };
+
+  logout = () => {
+    const { dispatch } = this.props;
+    setCookie('username', '');
+    setCookie('password', '');
+    dispatch({ type: 'auth/setauth', payload: { auth: -1 } });
+    router.push('/login');
+  };
+
   render() {
-    const { children } = this.props;
-    return (
+    const { children, history, auth } = this.props;
+    const { pathname } = history.location;
+    if (auth.auth === -1 && pathname !== '/login') {
+      console.log('not login!!');
+      message.error('请先登录');
+      router.push('/login');
+      return null;
+    } else if (auth.auth === 0) {
+      return null;
+    }
+    return pathname !== '/login' ? (
       <div className={styles.normal}>
         <Layout style={{ minHeight: '100vh' }}>
           <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
             <div className="logo" />
-            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-              {/* <Menu.Item key="1">
-                <Icon type="pie-chart" />
-                <span>Option 1</span>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <Icon type="desktop" />
-                <span>Option 2</span>
-              </Menu.Item>
-              <SubMenu
-                key="sub1"
-                title={
-                  <span>
-                    <Icon type="user" />
-                    <span>User</span>
-                  </span>
-                }
-              >
-                <Menu.Item key="3">Tom</Menu.Item>
-                <Menu.Item key="4">Bill</Menu.Item>
-                <Menu.Item key="5">Alex</Menu.Item>
-              </SubMenu>
-              <SubMenu
-                key="sub2"
-                title={
-                  <span>
-                    <Icon type="team" />
-                    <span>Team</span>
-                  </span>
-                }
-              >
-                <Menu.Item key="6">Team 1</Menu.Item>
-                <Menu.Item key="8">Team 2</Menu.Item>
-              </SubMenu>
-              <Menu.Item key="9">
-                <Icon type="file" />
-                <span>File</span>
-              </Menu.Item> */}
+            <Menu
+              onClick={this.onClickMenu}
+              theme="dark"
+              defaultSelectedKeys={MenuData.filter(data => data.path === pathname).map(data =>
+                String(data.index),
+              )}
+              mode="inline"
+            >
+              {MenuData.map(menu => (
+                <Menu.Item key={String(menu.index)}>
+                  <Icon type={menu.icon} />
+                  <span>{menu.name}</span>
+                </Menu.Item>
+              ))}
             </Menu>
           </Sider>
           <Layout>
-            <Header style={{ background: '#fff', padding: 0 }} />
+            <Header
+              style={{
+                background: '#fff',
+                padding: '0 20px',
+                display: 'flex',
+                justifyContent: 'flex-end',
+              }}
+            >
+              <a onClick={this.logout}>退出登录</a>
+            </Header>
             <Content style={{ margin: '0 16px' }}>
-              {/* <Breadcrumb style={{ margin: '16px 0' }}>
-                <Breadcrumb.Item>User</Breadcrumb.Item>
-                <Breadcrumb.Item>Bill</Breadcrumb.Item>
-              </Breadcrumb> */}
               <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>{children}</div>
             </Content>
           </Layout>
         </Layout>
       </div>
+    ) : (
+      children
     );
   }
 }
