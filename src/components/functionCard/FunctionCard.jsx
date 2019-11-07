7af55435007ab3771732bb92c470a1e4ad0482c5
dontdrinkmylemmontea <492861128@qq.com>
2019-11-07 17:32:30 +0800

    功能卡片，新增查看源代码、执行算法两个功能

--- src/components/functionCard/FunctionCard.jsx
+++ src/components/functionCard/FunctionCard.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Card, Icon } from 'antd';
 import styles from './FunctionCard.less';
 
-const algoWrapper = algo => array => {
+const algoWrapper = algo => (...args) => {
   const startTime = new Date().getTime();
-  const algoResult = algo(array);
+  const algoResult = algo(...args);
   let result;
   if (Array.isArray(algoResult)) {
     result = algoResult.join(' ');
@@ -13,34 +13,93 @@ const algoWrapper = algo => array => {
   }
   const endTime = new Date().getTime();
   const timeSpent = endTime - startTime;
-  return { timeSpent, result };
+  return { timeSpent, value: result };
 };
 
-const FunctionCard = ({ title, children, algo, array }) => (
-  <Card
-    style={{ width: 300, marginBottom: '30px', marginRight: '20px' }}
-    actions={[<Icon type="caret-right" key="caret-right" />]}
-    title={title}
-  >
-    {children}
-    {algo ? <div className={styles.sourceCode}>{algo.toString()}</div> : null}
-    {(() => {
-      let result = {};
-      if (algo) {
-        const algoTimmer = algoWrapper(algo);
-        result = algoTimmer(array || []);
-        console.log('result = ', result);
-      }
-      return (
-        <>
-          <br />
-          执行结果：{result.result || '--'}
-          <br />
-          执行时间：{result.timeSpent} s
-        </>
-      );
-    })()}
-  </Card>
+const LabelText = ({ label, value }) => (
+  <div>
+    {label}:&nbsp;&nbsp;&nbsp;{value}
+  </div>
 );
+
+class FunctionCard extends Component {
+  state = {
+    codeVisible: false,
+    result: {},
+    running: false,
+  };
+
+  componentDidMount() {
+    this.runArgs();
+  }
+
+  runArgs = () => {
+    this.setState({
+      running: true,
+    });
+    const { algo, array, args } = this.props;
+    let result = {};
+    if (algo) {
+      const algoTimmer = algoWrapper(algo);
+      result = args ? algoTimmer(...args) : algoTimmer(array);
+    }
+    this.setState({
+      result,
+      running: false,
+    });
+  };
+
+  render() {
+    const { title, children, algo, array, args } = this.props;
+    const {
+      codeVisible,
+      result: { value, timeSpent },
+      running,
+    } = this.state;
+    return (
+      <div className={styles.root}>
+        <Card
+          style={{ width: '700px' }}
+          actions={[
+            <Icon
+              type={running ? 'loading' : 'caret-right'}
+              key="caret-right"
+              style={{ color: running ? '#1890ff' : 'auto' }}
+              onClick={this.runArgs}
+            />,
+            <Icon
+              type={codeVisible ? 'eye-invisible' : 'eye'}
+              key="eye"
+              onClick={() => this.setState(pre => ({ codeVisible: !pre.codeVisible }))}
+            />,
+          ]}
+          title={title}
+        >
+          <LabelText
+            label={'入参'}
+            value={'[ ' + args ? (args || []).join(' ') : (array || []).join(',') + ' ]'}
+          />
+          {children}
+          <LabelText label={'耗时'} value={timeSpent + ' ms'} />
+          <LabelText label={'执行结果'} value={value} />
+        </Card>
+        {algo && codeVisible ? (
+          <Card>
+            <div
+              className={styles.sourceCode}
+              dangerouslySetInnerHTML={{
+                __html: algo
+                  .toString()
+                  .replace(/\n/g, '<br/>')
+                  .replace(/\t/g, '&nbsp;&nbsp;&nbsp;')
+                  .replace(/ /g, '&nbsp;'),
+              }}
+            ></div>
+          </Card>
+        ) : null}
+      </div>
+    );
+  }
+}
 
 export default FunctionCard;
--- src/pages/binarysearch/BinarySearch.jsx
+++ src/pages/binarysearch/BinarySearch.jsx
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import FunctionCard from 'components/functionCard';
 import styles from './BinarySearch.less';
 
-const binarySearch = source => {
-  return source;
+const binarySearch = (target, source) => {
+  function search(start, end, target, source) {
+    const middleIndex = Number(Number(Number((end - start) / 2) + Number(start)).toFixed());
+    const middleValue = source[middleIndex];
+    if (middleValue === target) return middleIndex;
+    if (middleIndex <= 1) return '没有找到';
+    if (middleValue > target) return search(start, middleIndex, target, source);
+    if (middleValue < target) return search(middleIndex, end, target, source);
+  }
+  return search(0, source.length, target, source);
 };
 
 const arr = [1, 4, 5, 6, 8];
@@ -12,7 +20,7 @@ class BinarySearch extends Component {
   render() {
     return (
       <div className={styles.root}>
-        <FunctionCard title="二分搜索" algo={binarySearch} array={arr} />
+        <FunctionCard title="二分搜索" algo={binarySearch} args={[5, arr]} />
       </div>
     );
   }
