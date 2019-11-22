f2facbaa5dab105dec4031a4b753b0fc27ef17f2
dontdrinkmylemmontea <492861128@qq.com>
2019-11-22 14:43:41 +0800

    增加下拉加载、更换图片源

--- src/pages/waterfall/WaterFall.jsx
+++ src/pages/waterfall/WaterFall.jsx
@@ -1,26 +1,46 @@
 import React, { Component } from 'react';
-import { getRandomPicture } from '@/utils/picture';
+import { getRandomPicture, getCattyPic } from '@/utils/picture';
+import styles from './WaterFall.less';
+import InfiniteScroll from 'react-infinite-scroller';
+import Loader from './Loader';
 // import { connect } from 'dva';
 // import {Spin} from 'antd';
-import styles from './WaterFall.less';
 
 const columnNum = 3;
+
+const initArray = (initItem = 0) => {
+  const newArr = [];
+  for (let i = 0; i < columnNum; i += 1) {
+    newArr[i] = typeof initItem === 'function' ? initItem() : initItem;
+  }
+  console.log('newArr = ', newArr);
+  return newArr;
+};
 
 // @connect(({ loading }) => ({
 //   loadingData: loading,
 // }))
 class WaterFall extends Component {
-  state = {
-    images: [[], [], []],
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: initArray(() => []),
+    };
+    this.columnHeight = initArray();
+  }
 
   componentDidMount() {
     for (let i = 0; i < columnNum; i += 1) {
-      getRandomPicture(src => this.setPictureSrc(i, src));
-      getRandomPicture(src => this.setPictureSrc(i, src));
-      getRandomPicture(src => this.setPictureSrc(i, src));
+      getCattyPic(src => this.setPictureSrc(i, src));
+      getCattyPic(src => this.setPictureSrc(i, src));
+      getCattyPic(src => this.setPictureSrc(i, src));
     }
   }
+
+  imgOnLoad = (e, column) => {
+    this.columnHeight[column] += e.currentTarget.height;
+    console.log('this.columnHeight = ', this.columnHeight);
+  };
 
   setPictureSrc = (index, src) => {
     this.setState(({ images }) => {
@@ -33,17 +53,37 @@ class WaterFall extends Component {
     });
   };
 
+  onLoadMore = page => {
+    console.log('load more');
+  };
+
   render() {
     const { images } = this.state;
     return (
       <div className={styles.root}>
-        {images.map((column, columnIndex) => (
-          <div className={styles.column} key={columnIndex}>
-            {column.map((src, index) =>
-              src ? <img src={src} key={index} alt={`pic-${index}`} /> : null,
-            )}
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={this.onLoadMore}
+          hasMore={true || false}
+          loader={<Loader />}
+        >
+          <div className={styles.container}>
+            {images.map((column, columnIndex) => (
+              <div className={styles.column} key={columnIndex} id={`column-${columnIndex}`}>
+                {column.map((src, index) =>
+                  src ? (
+                    <img
+                      src={src}
+                      key={index}
+                      alt={`pic-${index}`}
+                      onLoad={e => this.imgOnLoad(e, columnIndex)}
+                    />
+                  ) : null,
+                )}
+              </div>
+            ))}
           </div>
-        ))}
+        </InfiniteScroll>
       </div>
     );
   }
--- src/utils/picture.js
+++ src/utils/picture.js
@@ -1,46 +1,25 @@
-import https from 'https';
-const getKey = 'Cat';
-// const getKey = 'BingEverydayWallpaperPicture';
+import request from 'request';
 
-const imgGettingUrl = `https://uploadbeta.com/api/pictures/random/?key=${getKey}`;
+const catteGettingUrl = 'https://api.thecatapi.com/v1/images/search';
+const apiKey = 'bcf34e0b-3f3c-4ee6-8e3b-12aab033c63a';
 
-// 获取随机图片
-export const getRandomPicture = (callback, preCall) => {
+export const getCattyPic = (callback, preCall) => {
   if (preCall) {
     preCall();
   }
-
-  let counter = 0;
-  const maxRetryTimes = 5;
-
-  function httpsGet() {
-    const request = https.get(imgGettingUrl, response => {
-      const datas = [];
-      let size = 0;
-      counter += 1;
-      response.on('data', d => {
-        datas.push(d);
-        size += d.length;
-      });
-
-      response.on('end', () => {
-        const buff = Buffer.concat(datas, size);
-        const pic = buff.toString('base64');
-        const src = `data:image/jpeg;base64,${pic}`;
-        if (callback) {
-          callback(src);
-        }
-      });
-    });
-
-    request.on('error', e => {
-      if (counter < maxRetryTimes) {
-        setTimeout(() => {
-          httpsGet();
-        }, 300);
+  request(
+    {
+      url: catteGettingUrl,
+      headers: {
+        'x-api-key': apiKey,
+      },
+    },
+    (error, response, body) => {
+      const bodyObj = JSON.parse(body);
+      const src = bodyObj[0].url;
+      if (callback) {
+        callback(src);
       }
-    });
-  }
-
-  httpsGet();
+    },
+  );
 };
